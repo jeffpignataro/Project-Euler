f47c07f958252864c14a2c36e13d04ed91e601c2
Jeff Pignataro <jeff.pignataro@sa.ucsb.edu>
2019-08-24 23:08:30 -0600

    Ah sqrt of number to find divisors

--- 10/sum-of-primes.py
+++ 10/sum-of-primes.py
@@ -1,11 +1,14 @@
-from ProjectEuler.helper import is_prime
 import sys
-print(sys.path)
-#import helper
+import math
 
-n=int(input("Count to...? "))
-p=2
-total=0
-#for p in range(2, n+1):
-#    if(helper.is_prime(p)):
-#        print (total)
+n = 2000000
+total = counter = 0
+for p in range(2, n+1):
+    for i in range(2, int((math.sqrt(p))+1)):
+        if p % i == 0:
+            break
+    else:
+        counter += 1
+        total += p
+        print('{} {}'.format(counter, total))
+print('Done')
